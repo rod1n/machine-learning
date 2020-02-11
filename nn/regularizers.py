344d9620e92ed77685816a14de73da67acdf74ba
rod1n <59267124+rod1n@users.noreply.github.com>
2020-02-11 15:32:51 +0300

    Add L1L2 regularizer

--- nn/regularizers.py
+++ nn/regularizers.py
@@ -23,3 +23,16 @@ class L2(object):
 
     def get_grads(self, weights):
         return self.l * weights / 2
+
+
+class L1L2(object):
+
+    def __init__(self, l1, l2):
+        self.l1 = L1(l1)
+        self.l2 = L2(l2)
+
+    def get_penalty(self, weights):
+        return self.l1.get_penalty(weights) + self.l2.get_penalty(weights)
+
+    def get_grads(self, weights):
+        return self.l1.get_grads(weights) + self.l2.get_grads(weights)
