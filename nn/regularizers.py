77598d9e80cc59df16ed2ce41555d55350e38cbc
rod1n <59267124+rod1n@users.noreply.github.com>
2020-02-11 13:54:12 +0300

    Add L1 regularizer

--- nn/regularizers.py
+++ nn/regularizers.py
@@ -1,6 +1,18 @@
 import numpy as np
 
 
+class L1(object):
+
+    def __init__(self, l):
+        self.l = l
+
+    def get_penalty(self, weights):
+        return self.l * np.sum(np.abs(weights))
+
+    def get_grads(self, weights):
+        return self.l * np.sign(weights)
+
+
 class L2(object):
 
     def __init__(self, l):
